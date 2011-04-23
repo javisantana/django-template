2f8856e427545ab7aec5f93b960697b32be6dd96
vagrant <vagrant@vagrantup.com>
2011-04-23 09:44:23 -0700

    added production settings

---
+++ deploy/local_settings.py
@@ -0,0 +1,12 @@
+
+
+DATABASES = {
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',          
+        'USER': 'root', 
+        'PASSWORD': 'root',
+        'HOST': '',       
+        'PORT': '',      
+   }
+}
